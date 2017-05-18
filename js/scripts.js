80483e4140b86f3b41991f0b76aadc94c1388b02
lkneeshaw <lkneeshaw@vcd-ceb131-12.eastern.ewu.edu>
2017-05-18 16:37:12 -0700

    Version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,45 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
+// @codekit-prepend "airtable.js";
 
-console.log('Hello, World!');
+//Configure & Handshake
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyNyK2dVB4Da7Dzo'
+});
+var base = Airtable.base('appYjgGyxE7AXQYex');
+
+//check check
+console.log(base);
+
+// Get records
+base('YaHURR').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+    if (err) { console.error(err); return; }
+    records.forEach(function(record) {
+        
+//        console.log('Retrieved', record.get('Name'));
+        var students = `<h1>${record.get('Name')}</h1>`
+        
+        // Display
+        $('#students').html(students);
+        
+    });
+});
+
+var students = `          
+    <section class="ui card">
+          <div class="image">
+              <img src="https://placehold.it/400x300" alt="">
+          </div>
+
+          <div class="content">
+              <h3>First Last</h3>
+              <p class="description">
+                  <a href="">GitHub</a> and <a href="">Email</a>
+              </p>
+          </div>
+      </section>
+`;
